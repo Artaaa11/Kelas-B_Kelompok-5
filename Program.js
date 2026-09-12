caca89219d5eb5824ef7d420bea29ef5b5a01739
Juni Artaida <2610312045@mahasiswa.upnvj.ac.id>
2026-09-12 15:20:17 +0700

    Update Program.js

--- Program.js
+++ Program.js
@@ -1,1 +1,31 @@
+START
 
+Set angkaRahasia secara acak
+Set jumlahPercobaan = 0
+Set maksimalPercobaan = 5
+
+WHILE jumlahPercobaan < maksimalPercobaan
+
+    Minta pengguna memasukkan tebakan
+    Tambahkan 1 ke jumlahPercobaan
+
+    IF tebakan = angkaRahasia THEN
+        Tampilkan "Tebakan benar!"
+        Tampilkan jumlahPercobaan
+        Tampilkan "Kamu menang!"
+        STOP
+    ELSE IF tebakan < angkaRahasia THEN
+        Tampilkan "Terlalu kecil"
+    ELSE
+        Tampilkan "Terlalu besar"
+    END IF
+
+    Tampilkan sisa percobaan
+
+END WHILE
+
+Tampilkan "Kesempatan habis"
+Tampilkan "Kamu kalah"
+Tampilkan angkaRahasia
+
+END
